3ec853a227f2797e7408e19d7e2da5e97cbe1c8e
Laboratório FIAP <labsfiap@fiap.com.br>
2025-09-15 20:47:51 -0300

    feat: create loading and error-fallback

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,32 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { Home } from "./pages/home";
-import { AddWorkout } from "./pages/add-workout";
 import { Layout } from "./components/layout";
-import { WorkoutDetails } from "./pages/workout-details";
+import { lazy, Suspense } from "react";
+import { Loading } from "./components/loading";
+import { ErrorBoundary } from "react-error-boundary";
+import { ErrorFallback } from "./components/error-fallback";
+
+const Home = lazy(() => import("./pages/home").then(module => ({default: module.Home})))
+const AddWorkout = lazy(() => import("./pages/add-workout").then(module => ({default: module.AddWorkout})))
+const WorkoutDetails = lazy(() => import("./pages/workout-details").then(module => ({default: module.WorkoutDetails})))
+const NotFound = lazy(() => import("./pages/not-found").then(module => ({default: module.NotFound})))
+
+
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout/>}>
-          <Route index element={<Home/>}/>
-          <Route path="/add" element={<AddWorkout/>}/>
-          <Route path="/workout/:id" element={<WorkoutDetails/>}/>
-        </Route>
-      </Routes>
+      <ErrorBoundary FallbackComponent={ErrorFallback}>
+        <Suspense fallback={<Loading/>}>  
+          <Routes>
+            <Route path="/" element={<Layout/>}>
+              <Route index element={<Home/>}/>
+              <Route path="/add" element={<AddWorkout/>}/>
+              <Route path="/workout/:id" element={<WorkoutDetails/>}/>
+              <Route path="*" element={<NotFound/>}/>
+            </Route>
+          </Routes>
+        </Suspense>
+      </ErrorBoundary>
     </BrowserRouter>
   );
 }
